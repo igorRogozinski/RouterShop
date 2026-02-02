8ae1974fafa1917208586185d890c4fc3972c81a
AdminIgor <AdminIgor@IGOR>
2026-02-02 18:23:22 +0100

    context and migrations

---
+++ RouterShop/Context/DBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RouterShop.Models;
+
+namespace RouterShop.Context
+{
+    public class DBContext:DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
