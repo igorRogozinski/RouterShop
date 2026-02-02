689f3dcf952a0edbaf796d23928a3af8fe813d05
AdminIgor <AdminIgor@IGOR>
2026-02-02 18:22:58 +0100

    services added

---
+++ RouterShop/Services/Abstractions/ProductService.cs
@@ -0,0 +1,45 @@
+﻿using RouterShop.Models;
+using RouterShop.Models.DTO;
+using RouterShop.Repositories.Abstractions;
+using RouterShop.Services.Interfaces;
+
+namespace RouterShop.Services.Abstractions
+{
+    public class ProductService : IProductService
+    {
+        ProductRepo _productRepo;
+        public ProductService(ProductRepo productRepo)
+        {
+            _productRepo = productRepo;
+        }
+        public Task<List<ProductDto>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ProductPaginated> GetProductsPaginated(int pageNumber, int pageSize)
+        {
+            var products = await _productRepo.GetPaginated(pageNumber,pageSize);
+            var paginatedProducts = products
+                .Select(p => new ProductDto
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Price = p.Price
+                })
+                .ToList();
+            return new ProductPaginated
+            {
+                Products = paginatedProducts,
+                PageSize = pageSize,
+                CurrentPage = pageNumber,
+                TotalPages = (int)Math.Ceiling((double)products.Count / pageSize)
+            };
+        }
+    }
+}
---
+++ RouterShop/Services/Interfaces/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using RouterShop.Models;
+using RouterShop.Models.DTO;
+
+namespace RouterShop.Services.Interfaces
+{
+    public interface IProductService
+    {
+        public Task<List<ProductDto>> GetAll();
+        public Task<Product> GetById(int id);
+        public Task<ProductPaginated> GetProductsPaginated(int pageNumber, int pageSize);
+    }
+}
