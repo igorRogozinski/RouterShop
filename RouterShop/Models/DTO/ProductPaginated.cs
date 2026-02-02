19d5d62ab21b307f0cf3e79159cdaadb31b6608d
AdminIgor <AdminIgor@IGOR>
2026-02-02 18:23:10 +0100

    modelsadded

---
+++ RouterShop/Models/DTO/ProductPaginated.cs
@@ -0,0 +1,10 @@
+﻿namespace RouterShop.Models.DTO
+{
+    public class ProductPaginated
+    {
+        public List<ProductDto> Products { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
