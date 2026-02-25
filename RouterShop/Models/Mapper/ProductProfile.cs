77a3491d2253304b02827b09802d36ed8da06d5c
AdminIgor <AdminIgor@IGOR>
2026-02-25 11:34:02 +0100

    Product mapper

---
+++ RouterShop/Models/Mapper/ProductProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using RouterShop.Models.DTO;
+
+namespace RouterShop.Models.Mapper
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductListDto>();
+        }
+    }
+}
