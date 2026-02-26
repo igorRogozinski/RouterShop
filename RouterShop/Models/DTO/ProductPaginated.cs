3b8b4d977f1ccd81b5cb9df5a5aa989e68df4eaa
AdminIgor <AdminIgor@IGOR>
2026-02-26 14:08:15 +0100

    category filed added into product paginated

--- RouterShop/Models/DTO/ProductPaginated.cs
+++ RouterShop/Models/DTO/ProductPaginated.cs
@@ -3,6 +3,7 @@
     public class ProductPaginated
     {
         public List<ProductListDto> Products { get; set; }
+        public int? CategoryId { get; set; } = null;
         public int PageSize { get; set; }
         public int CurrentPage { get; set; }
         public int TotalPages { get; set; }
