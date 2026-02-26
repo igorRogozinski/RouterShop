cc9a715f7727a7f75e4848a741810cd832a9f9c5
AdminIgor <AdminIgor@IGOR>
2026-02-26 14:07:56 +0100

    Product Image can have one default image

---
+++ RouterShop/Context/Configurations/ProductImageConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RouterShop.Models;
+
+namespace RouterShop.Context.Configurations
+{
+    public class ProductImageConfiguration : IEntityTypeConfiguration<ProductImage>
+    {
+        public void Configure(EntityTypeBuilder<ProductImage> builder)
+        {
+            builder
+                .HasIndex(pi => pi.ProductId)
+                .IsUnique()
+                .HasFilter("[DefaultImage] = 1");
+        }
+    }
+}
